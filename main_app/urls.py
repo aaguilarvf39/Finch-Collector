ceb50ff41384d699604c7ff8a4f4a143686e6397
Jesus Aguilar <aaguilarvf39@gmail.com>
2022-08-09 12:38:17 -0700

    Part 2 done

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
-  path('gundams/', views.gundams_index, name='index'), 
+  path('gundams/', views.gundams_index, name='index'),
+  path('gundams/<int:gundam_id>/', views.gundams_detail, name='detail'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,18 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-class Gundam:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, name, skill, description, year):
-    self.name = name
-    self.skill = skill
-    self.description = description
-    self.year = year
-
-gundams = [
-  Gundam('Deathsythe Hell', 'Stealth', 'Variant of the grim reaper', 195),
-  Gundam('Heavyarms', 'Firepower', 'Suited for long range tactics', 195),
-  Gundam('Sandrock', 'Desert Specialist', 'Designed as commander due to strengths in advanced communications/ analysis systems', 195)
-]
+from .models import Gundam
 
 # Create your views here.
 def home(request):
@@ -22,6 +9,10 @@ def about(request):
   return render(request, 'about.html')
 
 def gundams_index(request):
-    return render(request, 'gundams/index.html', {
-        'gundams': gundams
-    }) 
+    gundams = Gundam.objects.all()
+    return render(request, 'gundams/index.html', {'gundams': gundams })
+
+
+def gundams_detail(request, gundam_id):
+   gundam = Gundam.objects.get(id=gundam_id)
+   return render(request, 'gundams/detail.html', { 'gundam': gundam })
